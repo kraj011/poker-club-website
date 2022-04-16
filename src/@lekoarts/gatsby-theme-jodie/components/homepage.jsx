1df40b2beac37e40728e6922eeb3b96efa47b98c
David Krajewski <davidkrajewski@yahoo.com>
2022-04-16 18:28:07 -0400

    Updated homepage

---
+++ src/@lekoarts/gatsby-theme-jodie/components/homepage.jsx
@@ -0,0 +1,13 @@
+import * as React from "React" 
+import Layout from "@lekoarts/gatsby-theme-jodie/src/components/layout"
+
+
+const Homepage = () => {
+    return(
+        <Layout>
+          <h1>CMU Poker Club</h1>
+        </Layout>
+    )
+}
+
+export default Homepage;
